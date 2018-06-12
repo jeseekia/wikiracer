ba531b367be3472a35289093dc029f58de511873
rarhoade <rarhoade@umich.edu>
2018-06-11 21:04:12 -0400

    finished functionality of GUI implementation

--- ex.py
+++ ex.py
@@ -20,20 +20,35 @@ def sayhi(entries):
     print(str(entries['Article 1'].get()))
     print(str(entries['Article 2'].get()))
 
-def trycall():
-    output = subprocess.Popen('python3 main.py \'Wild Yak\' \'Phil McGraw\'',shell=True, stdout=subprocess.PIPE)
-    print(output.stdout.read())
-    #print(output.find(']'))
-    #testarray = ast.literal_eval(output)
-    #print(testarray)
-    #print(output)
+def trycall(e):
+    #output = subprocess.call(['python3 main.py \'Wild yak\' \'Phil McGraw\' > output.txt', '-1'], shell=True)
+    output = subprocess.call(['python3 main.py \'' + str(e['Article 1'].get()) + '\' \'' + str(e['Article 2'].get()) + '\' > output.txt', '-1'], shell=True)
+    file = open("output.txt", "r")
+    parser = file.read()
+    #print(parser)
+    parser = parser[0 : parser.find("]") + 1]
+    testarray = ast.literal_eval(parser)
+    box_text = ""
+    for i in testarray:
+        #print(i)
+        box_text += str(i) + ", "
+    popupmessage(box_text[0 : len(box_text) - 2])
+
+def popupmessage(path):
+    popup = Tk()
+    popup.wm_title("Your Results")
+    label = Label(popup, text= path)
+    label.pack(side="top", fill="x", padx = 10, pady=10)
+    B1 = Button(popup, text="Done!", command=popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
 top = Tk()
+top.wm_title("Wikipedia Golf")
 fields = ('Article 1', 'Article 2')
 ents = makeform(top, fields)
 top.bind('<Return>', (lambda event, e=ents: fetch(e)))
-b1 = Button(top, text='Activate', command=(lambda e=ents: sayhi(e)))
-b1.pack(side=LEFT, padx=5, pady=5)
-b2 = Button(top, text='Try Command Prompt', command=(lambda: trycall()))
+b2 = Button(top, text='Find the Link', command=(lambda e=ents: trycall(e)))
 b2.pack(side=LEFT, padx=5, pady=5)
 top.mainloop()
 
